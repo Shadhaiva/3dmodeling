b91bd2e7ab8e0e6674442485ac645c6de898327e
Shadhaiva Kunwar <shadhaiva.kunwar023@apexcollege.edu.np>
2025-07-26 02:33:52 +0545

    Changes Made

--- customization/tailwind.config.ts
+++ customization/tailwind.config.ts
@@ -1,11 +1,11 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {
 			backgroundImage: {
-				'main-img': "url('/bg.png')",
+				'main-img': "url('customization/public/bg.png')",
 			},
 		},
 	},
